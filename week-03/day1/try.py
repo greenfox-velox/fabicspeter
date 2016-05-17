a983719848622802d4df6535843e6c65bb9f9c6f
Fabics Peter <fapqabt@gmail.com>
2016-05-17 14:47:30 +0200

    some relation added

--- week-03/day1/try.py
+++ week-03/day1/try.py
@@ -8,8 +8,23 @@ c = 44
 c = 44 * 2
 print (c)
 e = 8
-e **= 2
+e **= 3
+# e = e ** 3
 print(e)
 f = 16
-f *= 0.5
+f = f ** (1 / 2)
 print(f)
+g1 = 123
+g2 = 345
+print(g1 > g2)
+h1 = 350
+h2 = 200
+print(h2 * 2 > h1)
+i = 1357988018575474
+io = i % i
+print( io <= 1)
+j1 = 10
+j2 = 3
+print (j2 **3 > j1 > j2 ** 2 )
+k = 1521
+print ((k % 3 == 0) or (k % 5 == 0))
