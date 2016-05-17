11b5585b04da175900a5aee74e17beb868bbab07
Fabics Peter <fapqabt@gmail.com>
2016-05-17 15:31:15 +0200

    new save

--- week-03/day1/try.py
+++ week-03/day1/try.py
@@ -21,10 +21,20 @@ h1 = 350
 h2 = 200
 print(h2 * 2 > h1)
 i = 1357988018575474
-io = i % i
-print( io <= 1)
+print( i % 11)
 j1 = 10
 j2 = 3
 print (j2 **3 > j1 > j2 ** 2 )
 k = 1521
 print ((k % 3 == 0) or (k % 5 == 0))
+l = 'python'   # Tell the 2nd letter of the word
+print(l[1])
+m = 'Apple'   # fill the m variable with its cotnent 4 times
+print( m * 4)
+n = 'The result is: '
+a = 2
+b = 3   # concat the product of a and b to the n string
+n = n + str(a) + str (b)
+print (n)
+o = "pneumonoultramicroscopicsilicovolcanoconiosis"  # tell how many letters in o
+print(len(o))
