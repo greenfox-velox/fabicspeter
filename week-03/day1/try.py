63569d2d8bcbb9b3a24429890471f6e68c52bffa
Fabics Peter <fapqabt@gmail.com>
2016-05-17 16:01:52 +0200

    16-20

--- week-03/day1/try.py
+++ week-03/day1/try.py
@@ -34,7 +34,23 @@ print( m * 4)
 n = 'The result is: '
 a = 2
 b = 3   # concat the product of a and b to the n string
-n = n + str(a) + str (b)
+n = n + str(a * b)
 print (n)
 o = "pneumonoultramicroscopicsilicovolcanoconiosis"  # tell how many letters in o
 print(len(o))
+
+p1 = [1, 2, 3]
+p2 = [4, 5]
+# tell if p2 has more elements than p17
+print(len(p2) > len(p1))
+q = [4, 5, 6, 7]
+# get the 3rd element of q
+print (q[2])
+r = [54, 23, 66, 12]
+# add the second the third
+print(r[1] + r[2])
+
+s = [1, 2, 3, 8, 5, 6]
+# change the 8 to 4
+s[3] = 4
+print (s)
