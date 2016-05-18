c1ea5d90df3a20db179c3068341ee7504e467a6c
Fabics Peter <fapqabt@gmail.com>
2016-05-18 10:51:50 +0200

    day2 begin

---
+++ week-03/day02/try.py
@@ -0,0 +1,46 @@
+# print the numbers till ad from 0
+ad = 6
+while ad >= 0:
+    print(ad)
+    ad -= 1
+
+ae = 4
+text = 'Gold'
+a = 0
+while a != ae:
+    print (text)
+    a += 1
+# print content of the text variable ae times
+
+
+af = [4, 5, 6, 7]
+# print all the elements of af
+a = 0
+while a != len(af):
+    print (af[a])
+    a += 1
+
+ag = [3, 4, 5, 6, 7]
+# please double all the elements of the list
+b = 0
+while b != len(ag):
+    print ((ag[b]) * 2)
+    b += 1
+
+ah = ['kuty', 'macsk', 'cic']
+# add to all elements an 'a' on the end
+g = 0
+while g != len(ah):
+    print ((ah[g]) + 'a')
+    g += 1
+
+ah = [3, 4, 5, 6, 7]
+# print the sum of all numbers in ah
+total = 0
+a = 0
+while a != len(ah):
+    total += ah[a]
+    a += 1
+print (total)
+
+# print the even numbers till 20
