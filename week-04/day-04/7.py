fff95755f5fdde6687f8c3590717eac6c2ac0948
Fabics Peter <fapqabt@gmail.com>
2016-05-26 14:49:09 +0200

    7.py remade

--- week-04/day-04/7.py
+++ week-04/day-04/7.py
@@ -1,12 +1,12 @@
 # 7. Given a string, compute recursively (no loops) a new string where all the
 # lowercase 'x' chars have been changed to 'y' chars.
 
-def bunnie(b):
-    if b > 0 and b % 2 == 0:
-        return 2 + bunnie(b - 1)
-    elif b > 0 and b % 2 == 1:
-        return 3 + bunnie(b - 1)
+def work(b):
+    if '' == b:
+        return ''
+    elif b[0] == 'x':
+        return 'y' + work(b[1:])
     else:
-        return 0
+        return b[0] + work(b[1:])
 
-print (bunnie(5))
+print (work('xegdasifsmay'))
