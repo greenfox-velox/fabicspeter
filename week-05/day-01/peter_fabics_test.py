a268b7e16f900b4c4cff8ad6c034295dcc007eff
Fabics Peter <fapqabt@gmail.com>
2016-05-30 13:14:02 +0200

    done with testing

--- week-05/day-01/peter_fabics_test.py
+++ week-05/day-01/peter_fabics_test.py
@@ -1,8 +1,23 @@
 import unittest
-import basics
-import peter_fabics_work.py
+import peter_fabics_work
 
-class TestIO(unittest.TestCase):
+class TestCount_and_sorting_method(unittest.TestCase):
+        def test_sorting(self):
+            self.assertEqual(peter_fabics_work.sorting ('aa', 'aa'), True)
+            self.assertEqual(peter_fabics_work.sorting ('aa', 'ac'), False)
+            self.assertEqual(peter_fabics_work.sorting ('aa1A', '1aaa'), True)
+            self.assertEqual(peter_fabics_work.sorting ('cCc', 'aAa'), False)
+            self.assertEqual(peter_fabics_work.sorting ('cCc', 'CcC'), True)
+
+
+        def test_count(self):
+            #self.create_file_width_content(self.file_name, self.content)
+            self.assertEqual(peter_fabics_work.count ('c'), {'c':1})
+            self.assertEqual(peter_fabics_work.count ('assa'), {'a':2, 's':2})
+            self.assertEqual(peter_fabics_work.count ('c11'), {'c':1, '1':2})
+            self.assertEqual(peter_fabics_work.count ('cc12'), {'c':2, '1':1, '2':1})
+            self.assertEqual(peter_fabics_work.count ('bbb'), {'b':3})
+
 
 if __name__ == '__main__':
     unittest.main()
--- week-05/day-01/peter_fabics_work.py
+++ week-05/day-01/peter_fabics_work.py
@@ -1,29 +1,19 @@
-import unittest
-import basics
-import crypto_1dup
-
-class TestIO(unittest.TestCase):
-
 def sorting(x, y):
     x = x.upper()
     y = y.upper()
     if sorted(x) == sorted(y):
-        print True
+        return True
     else:
-        print False
+        return False
 
 sorting('kacsa', 'CSaka')
-
-
 
 def count(string):
     d = {}
     for w in string:
         d[w] = string.count(w)
     for k in sorted(d):
-        print (k + ' x ' + str(d[k]))
+        return d
+        #print (k + ' x ' + str(d[k]))
 
 count('ventillatorba')
-
-if __name__ == '__main__':
-    unittest.main()
