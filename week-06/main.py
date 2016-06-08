629a61f3c8d26de06af56f6a5ee0bdeb6bd721b7
fabicsp <fapqabt@gmail.com>
2016-06-08 10:13:56 +0200

    RPG project

--- week-06/main.py
+++ week-06/main.py
@@ -1,10 +1,104 @@
 from tkinter import *
 
 
-def main():
-
 root = Tk()
-canvas = Canvas(root, width='720', height='720')
+canvas = Canvas(root, width=720, height=1000, bg='white')
 canvas.pack()
 
-main()
+hero_image_down = PhotoImage(file='hero-down.png')
+hero_image_up = PhotoImage(file='hero-up.png')
+hero_image_left = PhotoImage(file='hero-left.png')
+hero_image_right = PhotoImage(file='hero-right.png')
+skeleton_image = PhotoImage(file='skeleton.png')
+boss_image = PhotoImage(file='boss.png')
+
+
+class Tile():
+    def __init__(self, x, y):
+        self.size = 72
+        self.row = y
+        self.column = x
+
+    def draw_tile(self, img):
+        canvas.create_image(self.row * self.size, self.column * self.size, image = img, anchor=NW)
+
+
+class FloorTile(Tile):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.floor_image = PhotoImage(file='floor.png')
+
+    def draw(self):
+        super().draw_tile(self.floor_image)
+
+
+class wall(Tile):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.wall_image = PhotoImage(file='wall.png')
+
+    def draw(self):
+        super().draw_tile(self.wall_image)
+
+
+class Character_hero():
+    def __init__(self, x, y):
+        self.size = 72
+        self.row = x
+        self.column = y
+
+    def draw_character(self, img):
+        canvas.create_image(self.column * self.size, self.row * self.size, image=img, anchor=NW)
+
+
+class HeroImage(Character_hero):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.hero_image = hero_image_down
+
+    def draw(self):
+        super().draw_character(self.hero_image)
+
+
+class GameMap():
+    def __init__(self, map):
+        self.map = map
+
+    def create_map_draw_list(self):
+        self.map_draw_list = []
+        for row in range(0, len(self.map)):
+            for column in range(0, len(self.map[row])):
+                if self.map[row][column] == 0:
+                    self.map_draw_list.append(FloorTile(row, column))
+                elif self.map[row][column] == 1:
+                    self.map_draw_list.append(wall(row, column))
+
+    def draw_game_map(self):
+        for i in self.map_draw_list:
+            i.draw()
+
+map_cordinates = (
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 1, 1, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0, 0, 0]
+)
+
+
+
+hero_spawn = [0, 0]
+
+level1 = GameMap(map_cordinates)
+level1.create_map_draw_list()
+level1.draw_game_map()
+
+hero = Character_hero(hero_spawn[0], hero_spawn[1])
+hero.draw_character(hero_image_down)
+
+root.mainloop()
--- week-06/map.py
+++ week-06/map.py
@@ -10,29 +10,77 @@ from tkinter import *
 root = Tk()
 canvas = Canvas(root, width='720', height='820')
 canvas.pack()
-picture1 = PhotoImage(file = 'floor.png')
-picture2 = PhotoImage(file = 'wall.png')
-class floor_painter():
-    def __init__(self, floor):
-        self.floor = picture1
-m = [
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-     [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-     ]
-for n in range(len(m)):
-    for i in range(len(m[n])):
-        if m[n][i] == 0:
-            canvas.create_image(i * 72,n * 72, image = picture1, anchor = NW)
-        else:
-            canvas.create_image(i * 72, n * 72, image = picture2, anchor = NW)
+# picture1 = PhotoImage(file = 'floor.png')
+# picture2 = PhotoImage(file = 'wall.png')
+class background():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+    def paint_bckgr(self, canvas):
+        canvas.create_image(self.y * 72,self.x * 72, image = self.img, anchor = NW)
+
+class floor(background):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.img = PhotoImage(file= 'floor.pmg')
+
+class wall(background):
+    def __init__(self, x, y):
+        self.img = PhotoImage(file = 'wall.png')
+
+
+class board():
+    def __init__(self, canvas, field_input):
+        self.canvas = canvas
+        self.field_input = field_input
+
+    def create_field(self):
+        self.output = []
+        for i in range(len(self.field_input)):
+            for k in range(len(self.field_input[i])):
+                if self.field_input[i][k] == 1:
+                    self.output(Floor(i,k))
+                else:
+                    self.outputappend(Wall(i,k))
+
+    def make_a_board(self):
+        for i in self.output:
+            i.draw(self.canvas)
+
+
+# class floor_painter():
+#     def __init__(self, x, y):
+#         self.floor = PhotoImage(file = 'floor.png')
+#
+# class wall_painter():
+#     def __init__(self, wall):
+#         self.wall = PhotoImage(file = 'wall.png')
+
+# def main():
+#     root = Tk()
+#     canvas = Canvas(root, width 720, height = 720)
+#     canvas.pack()
+
+    m = [
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+         [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],]
+# for n in range(len(m)):
+#     for i in range(len(m[n])):
+#         if m[n][i] == 0:
+#             canvas.create_image(i * 72,n * 72, image = self.floor, anchor = NW)
+#         else:
+#             canvas.create_image(i * 72, n * 72, image = self.wall, anchor = NW)
+    level1 = board(canvas, m)
+    level1.create_field()
+    level1.make_a_board()
 
 root.mainloop()
