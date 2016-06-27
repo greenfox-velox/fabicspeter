9c6e4c2d9218a2f32dcc918a426246714e7a720a
fabicsp <fapqabt@gmail.com>
2016-06-27 16:54:07 +0200

    21- 29

---
+++ week-07/day-01/27.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var z = 9;
+// if z is between 10 and 20 print 'Sweet!'
+// if less than 10 print 'More!',
+// if more than 20 print 'Less!'
+if (20 > z && z > 10){
+  console.log('Sweet!');
+} else if( z < 10){
+  console.log('More!');
+} else {
+  console.log('Less!');
+}
