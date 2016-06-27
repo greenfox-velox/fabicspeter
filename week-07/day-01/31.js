32fbbd8ea3be63df0a37b04332d2499212015b49
fabicsp <fapqabt@gmail.com>
2016-06-27 17:09:03 +0200

    30-33

---
+++ week-07/day-01/31.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var ad = 6;
+// print the numbers till ad from 0
+
+while (ad >= 0) {
+  console.log(ad);
+  ad -= 1;
+}
---
+++ week-07/day-01/33.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var af = [4, 5, 6, 7];
+// print all the elements of af
+var n = 0
+while (af[n]) {
+  console.log(af[n]);
+  n += 1;
+}
