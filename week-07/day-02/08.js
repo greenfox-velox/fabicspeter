5fb76a11d85552069617572257138642357db851
fabicsp <fapqabt@gmail.com>
2016-06-28 16:26:04 +0200

    8-9 done

---
+++ week-07/day-02/08.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var students = [
+  {name: 'Rezso', age: 9.5, candies: 2},
+  {name: 'Gerzson', age: 10, candies: 1},
+  {name: 'Aurel', age: 7, candies: 6},
+  {name: 'Zsombor', age: 12, candies: 5},
+  {name: 'Olaf', age: 12, candies: 7},
+  {name: 'Teodor', age: 3, candies: 2}
+];
+
+
+// create a function that counts the students that
+// has more than 4 candies
+
+var each_counter = 0
+function over_candies(students){
+  students.forEach(function(e){
+  if (e.candies > 4){
+    each_counter ++
+  }});
+  return each_counter
+}
+console.log(over_candies(students));
---
+++ week-07/day-02/09.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// create a function that takes a string and counts its letters
+// it should return an object thats keys are the letters and the values are
+// the counts.
+// example: "apple" -> {a: 2, p: 2, l: 1}
+function letter_counter(s){
+  var letters = {}
+  s.split('').forEach(function(e){
+    if (!(e in letters)){
+      letters[e] = 1
+    }else{
+      letters[e] ++
+    }
+  });return letters
+}
+console.log(letter_counter('appla'));
