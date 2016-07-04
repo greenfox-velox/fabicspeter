8acf4d63cdf3ff83b2a23296d32f476e8502472b
fabicsp <fapqabt@gmail.com>
2016-07-04 12:46:26 +0200

    bookmarklet

---
+++ week-07/day-03/bookmarklet-1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// var h1_replacer = document.querySelectorAll('h1');
+// h1_replacer.textContent = 'Green Fox Academy Conquers the World';
+//
+// for (var i = 0; i < h1_replacer.length; i ++){
+//   h1_replacer[i].textContent = 'Green Fox Academy Conquers the World'
+// };
+(function(){var h1_replacer = document.querySelectorAll('h1');
+h1_replacer.textContent = 'Green Fox Academy Conquers the World';
+for (var i = 0; i < h1_replacer.length; i ++){
+  h1_replacer[i].textContent = 'Green Fox Academy Conquers the World'};
+})()
