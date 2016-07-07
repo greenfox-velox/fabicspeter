e9e899e8d4b3ceae091db78a6c1574bc97ca8464
fabicsp <fapqabt@gmail.com>
2016-07-07 16:14:13 +0200

    html, js

---
+++ week-08/todo_app.js
@@ -0,0 +1,24 @@
+// 'use strict'
+//
+// var button1 = document.querySelector('.button-text');
+// var input = document.querySelector('.textinput')
+// var list_element = document.querySelector('ul')
+//
+// var xhr = new XMLHttpRequest();
+//
+// function button_push(){
+//   xhr.onload = function() {
+//     var data = xhr.responseText;
+//     var data = function list_all_element() {
+//       for(var i = 0; i < data.length; i++){
+//         list_element.textContent = data[i]
+//       }
+//     }
+//     // list_element.textContent = data;
+//   };
+//   xhr.open('GET', 'https://mysterious-dusk-8248.herokuapp.com/todos');
+//   xhr.setRequestHeader("X-Mashape-Key", "KEjoG3sQWtmshlaiwkAEob887fgSp1pi544jsniIOqOgC9nIXm");
+//   xhr.send();
+// };
+//
+// button1.addEventListener('click', button_push);
