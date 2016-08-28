a3ffbce83f6ba3430038d28faee090bce09b5e40
fabicsp <fapqabt@gmail.com>
2016-08-28 23:07:22 +0200

    going to heroku

--- logMeIn/index.js
+++ logMeIn/index.js
@@ -1,30 +1,46 @@
-'use strick';
+// 'use strict';
 
-var isItReady = true
-var maxTurns = 0
+var isItReady = true;
+var maxTurns = 0;
+
+var positions = {1:'', 2:'', 3:'', 4:'',5:'',6:'',7:'',8:'',9:''}
+// localStorage.setItem(1, "X")
+
+function getTheClosedInfos(tomb){
+  for(i = 1; i < 10; i++){
+    document.getElementById(i).innerHTML = localStorage.getItem(i);
+  }
+}
+getTheClosedInfos(positions)
 
 function changeInnerHTML(ID){
   maxTurns++;
-  if (document.getElementById(ID).innerHTML === "" && isItReady && maxTurns < 8){
+  // checkingLines()
+  if (document.getElementById(ID).innerHTML === "" && isItReady && maxTurns < 9){
     document.getElementById(ID).innerHTML = "X";
+    // names[ID] = "X"
+    localStorage.setItem(ID, "X")
     checkingLines()
     machineTurns()
   }
-  console.log(maxTurns);
+  // console.log(maxTurns);
 }
 
 function machineTurns(){
   var doc = document
   var randomPlace = Math.floor(Math.random() * 9) + 1
-  // checkingLines()
   if (doc.getElementById(randomPlace).innerHTML !== ""){
     machineTurns()
   }else if(doc.getElementById(5).innerHTML === ""){
     doc.getElementById(5).innerHTML = "O"
+    // names[5] = "O"
+    localStorage.setItem(5, "O")
   }
   else if (isItReady){
     doc.getElementById(randomPlace).innerHTML = "O"
+    localStorage.setItem(randomPlace, "O")
   }
+  checkingLines()
 }
 
 function checkingLines(){
@@ -41,13 +57,15 @@ function checkingLines(){
     seven == "X" && eight == "X" && nine == "X" || one == "X" && four == "X" && seven == "X"||
     two == "X" && five == "X" && eight == "X" || three == "X" && six == "X" && nine == "X"||
     one == "X"&& five == "X" && nine == "X"|| three == "X" && five == "X" && seven == "X" ){
-    console.log("nyertel")
+    document.getElementById("TextFooter").innerHTML = "Gratulation! You won!"
+    document.getElementById("IMG").setAttribute("src", "evelin.gif")
     isItReady = false
   }else if (one == "O"  && two == "O" && three == "O" || four == "O" && five == "O" && six == "O" ||
     seven == "O" && eight == "O" && nine == "O" || one == "O" && four == "O" && seven == "O"||
     two == "O" && five == "O" && eight == "O" || three == "O" && six == "O" && nine == "O"||
     one == "O"&& five == "O" && nine == "O"|| three == "O" && five == "O" && seven == "O"){
-    console.log("a gep nyert sorry");
+    document.getElementById("TextFooter").innerHTML = "Comupter Wins! Try it again!!"
+    document.getElementById("IMG").setAttribute("src", "evelinsad.gif")
     isItReady = false
   }
 }
